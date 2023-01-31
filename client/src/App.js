69422407df8248161d1afafcc33c52653865ad81
Chrisld50 <chrisld50@yahoo.com>
2023-01-30 21:55:45 -0600

    made corrections to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import Header from './components/header';
 
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
